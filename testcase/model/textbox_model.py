7606d3c68b41bcb4af8bc69354521de215f97a53
Felipe Barden <felipebarden@gmail.com>
2013-01-30 02:14:20 -0200

    [TestCases] Add a edit_testcase mode file: textbox.

---
+++ testcase/model/textbox_model.py
@@ -0,0 +1,16 @@
+import sys
+
+class textBox:
+	text = ""
+
+	def __init__(self) :
+		self.text = ""
+
+	def getText() :
+		return self.text
+
+	def setText(text) :
+		self.text = text
+
+	def updateHighlight() :
+		
